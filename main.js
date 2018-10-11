a4c938181f6cc86e114ce1f9362e449fbd553fbd
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2018-10-10 17:01:28 -0700

    added chip pieces, graphics

--- main.js
+++ main.js
@@ -31,6 +31,25 @@ var counterObj = {};
 var turnTrackerObj = {}
 var directionToCheck = null;
 
+// Score Board
+function keepScore(){
+    var tallyWhite = null;
+    var tallyBlack = null;
+    for( var i=0;i<gameBoardArray.length; i++){
+        for(var j=0;j<gameBoardArray[i].length;j++){
+            if (gameBoardArray[i][j] === 'b'){
+                tallyBlack++;
+            } else if (gameBoardArray[i][j] === 'w'){
+                tallyWhite++;
+            }
+        }
+    }
+    $('.pointsW').text(tallyWhite);
+    $('.pointsB').text(tallyBlack);
+}
+
+
+// Game Board Functions
 function buildGameBoardArray(){
     gameBoardArray = [
         ['', '', '', '', '', '', '', '' ],
@@ -61,8 +80,10 @@ function buildGameBoard(){
                 boardDiv.append(topDiv);
             }
             gameArea.append(boardDiv);
+
         }
     }
+    keepScore();
 }
 function applyClickHandlers(){
     $(".gameboard").on('click', '.gameboard-tile', handleBoardClick);
@@ -78,6 +99,7 @@ function resetBoard(){
     $('.gameboard').empty();
     buildGameBoardArray();
     buildGameBoard();
+    keepScore();
 }
 
 function handleBoardClick(){
@@ -175,6 +197,7 @@ function changePieces(direction){
     console.log(gameBoardArray)
     $('.gameboard').empty();
     buildGameBoard();
+
 }
 function switchTurns(){
     if(blackTurn){
